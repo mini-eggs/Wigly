ebc682d508de6d0fd66bfc495adade16842580c1
Evan M Jones <evanjones4040@gmail.com>
2018-10-26 16:45:21 -0700

    Slight modification to wigly-use

--- packages/wigly-use/src/main.js
+++ packages/wigly-use/src/main.js
@@ -3,33 +3,42 @@ var keys = ["mounted", "updated", "destroyed"];
 export var use = f => ({
   ["data"]: () => ({
     ["f"]: p => f(p),
-    ["values"]: new Map(),
-    ["lifecycles"]: new Map()
+    ["v"]: new Map(),
+    ["l"]: new Map()
   }),
 
   ...keys.reduce(
     (total, key) => ({
       ...total,
       [key](el) {
-        var f = this["state"]["lifecycles"]["get"](key);
+        var f = this["state"]["l"]["get"](key);
         f && f(el);
       }
     }),
     {}
   ),
 
+  ["use"](initial) {
+    return [
+      this["state"]["v"]["get"](initial) || initial,
+      value => {
+        this["state"]["v"]["set"](initial, value);
+        this["setState"]({});
+      }
+    ];
+  },
+
+  ["reg"](i) {
+    return f => this["state"]["l"]["set"](keys[i], f);
+  },
+
   ["render"]() {
     return this["state"]["f"]({
       ...this["props"],
-      ["useState"]: initial => [
-        this["state"]["values"]["get"](initial) || initial,
-        value => {
-          this["state"]["values"]["set"](initial, value), this["setState"]({});
-        }
-      ],
-      ["useMount"]: f => this["state"]["lifecycles"]["set"](keys[0], f),
-      ["useUpdate"]: f => this["state"]["lifecycles"]["set"](keys[1], f),
-      ["useDestroy"]: f => this["state"]["lifecycles"]["set"](keys[2], f)
+      ["useState"]: this["use"],
+      ["useMount"]: this["reg"](0),
+      ["useUpdate"]: this["reg"](1),
+      ["useDestroy"]: this["reg"](2)
     });
   }
 });
